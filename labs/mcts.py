6af865c6d7ed40877335b8eaad41db23fedf1964
BALDURS_LABTOP\baldu <bath@itu.dk>
2025-09-16 16:10:04 +0200

    my sol

--- labs/mcts.py
+++ labs/mcts.py
@@ -8,6 +8,7 @@ from dataclasses import dataclass, field
 
 # %% Setup
 env: Env
+amountOfVisted = 0
 
 
 # %%
@@ -23,37 +24,101 @@ def minimax(state: State, maxim: bool) -> int:
 
 
 def alpha_beta(state: State, maxim: bool, alpha: int, beta: int) -> int:
-    raise NotImplementedError  # you do this
+    if state.ended:
+        return state.point
+    else:
+        temp: int = -10 if maxim else 10
+        for action in np.where(state.legal)[0]:  # for all legal actions
+            value = alpha_beta(env.step(state, action), not maxim, alpha, beta)
+            temp = max(temp, value) if maxim else min(temp, value)
+            if maxim:
+                alpha = max (alpha, temp)
+                if beta <= alpha:
+                    break
+            else:
+                beta = min(beta, temp)
+                if beta <= alpha:
+                    break
+        return temp
+
 
 
 @dataclass
 class Node:
-    state: State  # Add more fields
+    def __init__(self, state : State, parent : Node, action : int):
+        self.state = state
+        self.parent = parent
+        self.visted = 0
+        self.reward = 0
+        self.action = action
+        self.children = []
 
 
 # Intuitive but difficult in terms of code
 def monte_carlo(state: State, cfg) -> int:
-    raise NotImplementedError  # you do this
+    v0 = Node(state, None, None)
+    for _ in range(cfg.compute):
+        v1 = tree_policy(v0, cfg)
+        delta = default_policy(v1.state)
+        backup(v1,delta)
+    return best_child(v0, cfg.c).action
 
 
 def tree_policy(node: Node, cfg) -> Node:
-    raise NotImplementedError  # you do this
+    while not node.state.ended:
+        a = untriedAction(node)
+        if a is not None:
+            return expand(node, a)
+        else:
+            node = best_child(node, cfg.c)
+    return node
 
+def untriedAction (v : Node) -> int:
+    legal = np.where(v.state.legal)[0]
+    tried = {child.action for child in v.children}
+    untried = [a for a in legal]
+    for a in legal:
+        if a in tried:
+            untried.remove(a)
+    if len(untried) <= 0:
+        return None
+    return untried[0]
 
-def expand(v: Node) -> Node:
-    raise NotImplementedError  # you do this
+def expand(v: Node, a : int) -> Node:
+    newChild = Node(env.step(v.state, a), v, a)
+    v.children.append(newChild)
+    return newChild
 
 
 def best_child(root: Node, c) -> Node:
-    raise NotImplementedError  # you do this
+    bestScore = 0
+    best = root.children[0]                    
+    for child in root.children:
+        score = UCB1(root, child)
+        if score > bestScore:
+            bestScore = score
+            best = child
+    return best
 
+def UCB1(root : Node, node : Node) -> float:
+    return node.reward + np.sqrt(np.log2(root.visted) / node.visted)
 
 def default_policy(state: State) -> int:
-    raise NotImplementedError  # you do this
+    while not state.ended:
+        actions = np.where(state.legal)[0]
+        if len(actions) == 0:
+            break
+        action = np.random.choice(actions)
+        state = env.step(state, action)
+    return state.point
 
 
 def backup(node, delta) -> None:
-    raise NotImplementedError  # you do this
+    while node is not None:
+        node.visted += 1
+        node.reward += delta
+        delta = -delta
+        node = node.parent
 
 
 # Main function
@@ -82,7 +147,7 @@ def main(cfg) -> None:
                 a = actions[np.argmax(values) if state.maxim else np.argmin(values)]
 
             case "monte_carlo":
-                raise NotImplementedError
+                a = monte_carlo(state, cfg)
 
             case _:
                 raise ValueError(f"Unknown player {state.player}")
